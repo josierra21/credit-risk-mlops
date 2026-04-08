20f88484b6ac99ada333c16dd496daa97275dc09
jsierr16 <jsierr16@wgu.edu>
2026-04-08 13:29:07 -0500

    added entities

--- demo.py
+++ demo.py
@@ -1,11 +1,3 @@
-from credit_risk.logger import logging
-from credit_risk.exception import CREDITriskException
-import sys
-
-
-logging.info("Welcome to our custom log")
-
-try: 
-    a = 2/0
-except Exception as e:
-    raise CREDITriskException(e, sys)
+import os
+mongo_db_url = os.getenv('MONGODB_URL')
+print(mongo_db_url)
